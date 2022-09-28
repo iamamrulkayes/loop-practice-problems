a8c92ac4a9b8780e15a34f829b7ec4b3c1204f3a
iamamrulkayes <kayesasojib@gmail.com>
2022-09-28 22:59:53 +0600

    first commit

---
+++ numbers.js
@@ -0,0 +1,6 @@
+var number = 1;
+// Target: Display 1 to 10
+while (number <= 10) {
+    console.log(number);
+    number++;
+}
---
+++ odd.js
@@ -0,0 +1,7 @@
+// Target: 1 to 20 all odd numbers
+var number = 1;
+while (number <= 100) {
+    console.log(number);
+    // number++;
+    number = number + 2;
+}
